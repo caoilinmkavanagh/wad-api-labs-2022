e6ff8b3e7dbf57471182d7bbd14b63366278a1aa
Caolin Kavanagh <caoilinmkavanagh@gmail.com>
2023-08-24 17:21:56 +0100

    Mongo lab Exercises done

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,4 +1,6 @@
 import userModel from '../api/users/userModel.js';
+import genreModel from '../api/genres/genreModel.js';
+import genres from './genres.js';
 import users from './users.js';
 import dotenv from 'dotenv';
 import dbConnection from '../db/index.js';
@@ -17,9 +19,20 @@ async function loadUsers() {
    }
 }
 
+async function loadGenres() {
+   console.log('load genre Data');
+   try {
+      await genreModel.deleteMany();
+      await genreModel.collection.insertMany(genres);
+      console.info(`${users.length} genres were successfully stored.`);
+   } catch (err) {
+      console.error(`failed to Load genre Data: ${err}`);
+   }
+}
+
 if (process.env.SEED_DB) {
    dbConnection
-      .then(loadUsers)
+      .then(loadUsers, loadGenres)
       .catch(err => {
          console.error(`Failed to connect to database: ${err}`);
       });
